1f41604152714971a75ebc7da5d5d4bbed6479c9
GOPI CH <ch.gopi7893@gmail.com>
2025-06-20 08:03:44 +0530

    Delete productservice-master directory

--- productservice-master/src/main/java/dev/Gopi/productservice/repositories/PriceRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.Gopi.productservice.repositories;
-
-import dev.Gopi.productservice.models.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceRepository
-extends JpaRepository<Price, Long> {
-}
